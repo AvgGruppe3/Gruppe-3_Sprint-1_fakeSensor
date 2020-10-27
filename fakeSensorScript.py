ad7c83f88e97b84a2c8e2917ff45b6dda3abee11
Philipp <deph1015@hs-karlsruhe.de>
2020-10-27 15:43:58 +0100

    added last will and retained message

--- fakeSensorScript.py
+++ fakeSensorScript.py
@@ -5,13 +5,15 @@ import uuid
 
 mqttBroker ="localhost"
 port = 1883
+topic = "hska/avg/temperature2"
 
 client = mqtt.Client(str(uuid.uuid4()))
+client.will_set(topic, -273.2, 1, True)
 client.connect(mqttBroker, port, 60) 
 
 while True:
     randNumber = uniform(5.0, 55.0)
     roundedNumber = "{:.2f}".format(round(randNumber, 2))
-    client.publish("hska/avg/temperature2", roundedNumber)
-    print("hska/avg/temperature2: " + roundedNumber)
+    client.publish(topic , roundedNumber)
+    print(f"{topic}: {roundedNumber}")
     time.sleep(5)
