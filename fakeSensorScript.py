5f6020f0f711053297671ad78c9b74fffc71ff5d
Philipp <deph1015@hs-karlsruhe.de>
2020-10-27 15:31:29 +0100

    new branche

--- fakeSensorScript.py
+++ fakeSensorScript.py
@@ -7,11 +7,12 @@ mqttBroker ="localhost"
 port = 1883
 
 client = mqtt.Client(str(uuid.uuid4()))
-client.connect(mqttBroker, port, 60) 
+client.will_set("hska/avg/temperature2", str(0.00), 0,True)
+client.connect(mqttBroker, port, 60)
 
 while True:
     randNumber = uniform(5.0, 55.0)
     roundedNumber = "{:.2f}".format(round(randNumber, 2))
-    client.publish("hska/avg/temperature2", roundedNumber)
+    client.publish("hska/avg/temperature2", roundedNumber, 1)
     print("hska/avg/temperature2: " + roundedNumber)
     time.sleep(5)
